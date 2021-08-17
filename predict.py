f4912e89fc4b950a488758aaf5e19516a03f32bf
Hoang Cao Bao <hoangcaobao@MacOnes-MacBook-Pro.local>
2021-08-18 01:20:20 +0700

    update predict

--- predict.py
+++ predict.py
@@ -1,24 +1,38 @@
 import os
+import tensorflow as tf
+from model import *
+import numpy as np
+from tensorflow.keras.preprocessing.image import *
+from tensorflow.keras.models import *
 from argparse import ArgumentParser
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--batch-size", default=64, type=int)
-    parser.add_argument("--epochs", default=1000, type=int)
+    parser.add_argument("--test-image", default="test.png", type=str, required=True)
+    parser.add_argument("--model-folder", default="model", type=str)
 
-    # FIXME
+   
     args = parser.parse_args()
 
-    # FIXME
-    # Project Description
 
-    print('---------------------Welcome to ${name}-------------------')
-    print('Github: ${accout}')
-    print('Email: ${email}')
+    print('---------------------Welcome to ResNet-------------------')
+    print('Github: hoangcaobao')
+    print('Email: caobaohoang03@gmail.com')
     print('---------------------------------------------------------------------')
-    print('Training ${name} model with hyper-params:') # FIXME
+    print('Predict using ResNet model for test file path {0}'.format(args.test_file_path)) # FIXME
     print('===========================')
 
-    # FIXME
-    # Do Training
+    print("===================LOADING MODEL==========================")
+    model=load_model(args.model_folder)
+    print("================FINISH LOADING MODE=======================")
 
+    print("===================LOADING TEST IMAGE=====================")
+    image=load_img(args.test_image)
+    input_arr=img_to_array(image)
+    input_arr=np.array([input_arr])
+    print("================FINISH LOADING TEST IMAGE==================")
+
+    print("===================PREDICTING=====================")
+    result=np.argmax(model.predict(input_arr),axis=1)
+    print("YOUR RESULT AFTER GO THROUGH RESNET {}".format(result))
+    print("==================END PREDICTING=====================")
