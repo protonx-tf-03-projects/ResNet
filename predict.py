c65bb30469558ca4b94718e8a5016fb617209f5a
Gia Huy <bdghuy@gmail.com>
2021-09-06 19:50:04 +0700

    get class names

--- predict.py
+++ predict.py
@@ -1,15 +1,16 @@
-import os
+from tensorflow.keras.models import load_model
+from tensorflow.keras import preprocessing
 import tensorflow as tf
-from model import *
-import numpy as np
-from tensorflow.keras.preprocessing.image import *
-from tensorflow.keras.models import *
 from argparse import ArgumentParser
+import numpy as np
+import pickle
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--test-image", default="test.png", type=str, required=True)
+    parser.add_argument("--test-file-path", type=str, required=True)
     parser.add_argument("--model-path", default="best_model.h5", type=str)
+    parser.add_argument("--class-names-path", default='class_names.pkl', type=str)
+
 
    
     args = parser.parse_args()
@@ -23,21 +24,22 @@ if __name__ == "__main__":
     print('2. Github: sonnymetvn')
     print('3. Github: hoangduc199891')
     print('4. Github: bdghuy')
-    print('---------------------------------------------------------------------')
+    print('-------------------------------------------------------- ')
     print('Predict using ResNet model for test file path {0}'.format(args.test_file_path)) # FIXME
     print('===========================')
 
-    print("===================LOADING MODEL==========================")
+    # Loading class names
+    with open (args.class_names_path, 'rb') as fp:
+      class_names = pickle.load(fp)
+
+    # Loading model
     model=load_model(args.model_path)
-    print("================FINISH LOADING MODE=======================")
 
-    print("===================LOADING TEST IMAGE=====================")
-    image=load_img(args.test_image)
-    input_arr=img_to_array(image)
-    input_arr=np.array([input_arr])
-    print("================FINISH LOADING TEST IMAGE=================")
+    # Load test images
+    image = preprocessing.image.load_img(arg.test_file_path, target_size=(224,224))
+    input_arr = preprocessing.image.img_to_array(image)/225
+    x = np.expand_dims(input_arr, axis=0)
 
-    print("======================PREDICTING==========================")
-    result=np.argmax(model.predict(input_arr),axis=1)
-    print("YOUR RESULT AFTER GO THROUGH RESNET {}".format(result))
-    print("====================END PREDICTING========================")
+    predictions = model.predict(x)
+    label=np.argmax(predictions)
+    print('Result: {}'.format(class_names[label]))
